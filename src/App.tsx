689fd8c57a130e284b549d437cd4a250d80acb24
Roberta Miosi <148369468+RobertaMi89@users.noreply.github.com>
2024-11-10 10:40:14 +0100

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import Header from "./components/template/Header"
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Header from "./components/template/Header";
 import ContactList from "./components/organisms/ContactList";
 import Footer from "./components/template/Footer";
+import { LanguageProvider } from "./components/LanguageContext";
+import ContactDetailPage from './components/pages/ContactDetailPage';
 
 function App() {
 
+  const location = useLocation();
+  const isDetailPage = location.pathname.startsWith("/contact/");
+  console.log("Current location:", location);
+
+
   return (
-    <div>
-      <Header />
-      <ContactList />
-      <Footer />
+    <div className="flex flex-col min-h-screen">
+      <LanguageProvider>
+
+        {!isDetailPage && <Header />}
+        <Routes>
+          <Route path="/contact/:id" element={<ContactDetailPage />} />
+          <Route path="/" element={<ContactList />} />
+        </Routes>
+        {!isDetailPage && <Footer />}
+
+      </LanguageProvider>
     </div>
   );
 }
