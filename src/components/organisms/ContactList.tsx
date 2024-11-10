921aea952f3ca7066ea8bf96bf319387a511972d
Roberta Miosi <148369468+RobertaMi89@users.noreply.github.com>
2024-11-10 10:40:47 +0100

    add router

--- src/components/organisms/ContactList.tsx
+++ src/components/organisms/ContactList.tsx
@@ -1,41 +1,69 @@
 import { useEffect, useState } from 'react';
-import { database } from '../../services/firebaseConfig';
+import { Link } from "react-router-dom";
+import { db } from '../../services/firebaseConfig';
 import { ref, onValue } from 'firebase/database';
+import { Contact } from "../../types/Contact";
+
 
 const ContactList: React.FC = () => {
-  const [contacts, setContacts] = useState<{ name: string; surname: string; phone: string; email: string }[]>([]);
+  const [contacts, setContacts] = useState<Contact[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    const contactsRef = ref(database, 'contacts');
+    const contactsRef = ref(db, 'contacts');
 
-    onValue(contactsRef, (snapshot) => {
-      const data = snapshot.val();
+    onValue(
+      contactsRef,
+      (snapshot) => {
+        const data = snapshot.val();
+        setLoading(false);
 
-      if (data) {
-        const formattedContacts = Object.values(data) as {
-          name: string;
-          surname: string;
-          phone: string;
-          email: string;
-        }[];
-
-        setContacts(formattedContacts);
-      } else {
-        setContacts([]);
+        if (data) {
+          const formattedContacts = Object.values(data) as Contact[];
+          setContacts(formattedContacts);
+        } else {
+          setContacts([]);
+        }
+      },
+      (err) => {
+        setLoading(false);
+        if (err) {
+          setError('Errore nel caricamento dei contatti');
+        }
       }
-    });
+    );
   }, []);
+
+  if (loading) {
+    return <div>Caricamento contatti...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
 
   return (
     <>
       <ul>
-        {contacts.map((contact, index) => (
-          <li key={index}>
-            <strong>
-              {contact.name} {contact.surname}
-            </strong>
-          </li>
-        ))}
+        {contacts.length > 0 ? (
+          contacts.map((contact, index) => (
+            <li key={index}>
+              <Link to={`/contact/${contact.id}`}>
+                <div className="contact-item">
+                  <img
+                    src={contact.image || "/default-avatar.png"}
+                    alt={contact.name}
+                    className="w-12 h-12 rounded-full"
+                  />
+                  <p>{contact.name} {contact.surname}</p>
+                </div>
+              </Link>
+            </li>
+          ))
+        ) : (
+          <li>Nessun contatto trovato.</li>
+        )}
       </ul>
     </>
   );
