d608086b06cb36a0719b1def9c5b9740c26d820f
Roberta Miosi <148369468+RobertaMi89@users.noreply.github.com>
2024-11-12 11:44:29 +0100

    add components

---
+++ src/components/atoms/ContactAvatar.tsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+
+interface ContactAvatarProps {
+    name: string;
+    image: string | null;
+    className?: string;
+}
+
+const generateRandomColor = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};
+
+const ContactAvatar = ({ name, image, className }: ContactAvatarProps) => {
+    const [bgColor, setBgColor] = useState<string>("#fff");
+
+    useEffect(() => {
+        if (!image) {
+            setBgColor(generateRandomColor());
+        }
+    }, [image]);
+
+    const initial = name ? name.charAt(0).toUpperCase() : "";
+
+    return image ? (
+        <img
+            src={image}
+            alt={name}
+            className={`w-12 h-12 rounded-full mt-2 object-cover ${className}`}
+        />
+    ) : (
+        <div
+            style={{ backgroundColor: bgColor }}
+            className={`w-12 h-12 rounded-full flex items-center justify-center mt-2 text-white text-lg font-semibold ${className}`}  // Aggiunta del className dinamico
+        >
+            {initial}
+        </div>
+    );
+};
+
+export default ContactAvatar;
